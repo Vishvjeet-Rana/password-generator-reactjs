13a1c28faf6aeddb5c1f9b9fe70677d32590fd85
Vishvjeet-Rana <aryanrana62366@gmail.com>
2025-01-06 13:15:56 +0530

    initial commit after setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="top-0 right-0 bottom-0 left-0 h-full w-full absolute bg-red-400">
+        <h1 className="text-center text-4xl">Password Genrator</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
